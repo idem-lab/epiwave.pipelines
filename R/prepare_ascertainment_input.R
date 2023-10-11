6b2eec19b337c99f86f71e94369e71544609f9ae
SeniorKate <senior.k@unimelb.edu.au>
2023-10-11 12:10:15 +1100

    Add functionality to make separate PCR and RAT CAR inputs and assumptions specific to these two test types (e.g. CAR of 0 after RAT reporting ceases in each state)

--- R/prepare_ascertainment_input.R
+++ R/prepare_ascertainment_input.R
@@ -2,7 +2,8 @@
 prepare_ascertainment_input <- function(
         dates, jurisdictions,
         ascertainment_estimate = NULL,
-        assume_constant_ascertainment = NULL) { # should be value
+        assume_constant_ascertainment = NULL,
+        test_type = c("PCR", "RAT"), rat_reporting = NULL) { # should be value
 
     # add option here likelihood for seroprevalence data to help constrain prior
 
@@ -17,7 +18,7 @@ prepare_ascertainment_input <- function(
                                       ncol = length(jurisdictions),
                                       dimnames = list(dates, jurisdictions))
     } else if (!is.null(ascertainment_estimate) & is.null(assume_constant_ascertainment)) {
-        ascertainment_input <- get_CAR_from_surveys(dates, jurisdictions, ascertainment_estimate)
+        ascertainment_input <- get_CAR_from_surveys(dates, jurisdictions, ascertainment_estimate, test_type, rat_reporting)
     }
 
     ascertainment_input
@@ -28,7 +29,7 @@ prepare_ascertainment_input <- function(
 # date_state_mat <- create_date_state_matrix()
 # latest_survey_data <- get_latest_survey_data_file()
 #
-get_CAR_from_surveys <- function (dates, jurisdictions, latest_survey_data) {
+get_CAR_from_surveys <- function (dates, jurisdictions, latest_survey_data, test_type = c("PCR", "RAT"), rat_reporting = NULL) {
 
     date_state <- dplyr::tibble(
         date = rep(dates, length(jurisdictions)),
@@ -37,32 +38,67 @@ get_CAR_from_surveys <- function (dates, jurisdictions, latest_survey_data) {
     CAR_time_point <- latest_survey_data |>
         readr::read_csv() |>
         dplyr::select(state, date, fitted_point4) |>
-        dplyr::rename('test_prob_given_symptom' = fitted_point4) |>
-        dplyr::mutate(test_prob_given_infection = test_prob_given_symptom * 0.75 * 0.01)
+        dplyr::mutate(test_prob_given_symptom = fitted_point4 * 0.01) #changed to a proportion at this point because rat reporting is a proportion
+
+
+     if(!is.null(rat_reporting)) {
+        RAT_report_time_point <- rat_reporting |>
+             readr::read_csv() |>
+             dplyr::select(state, date, proportion)
+
+        RAT_CAR_smooth <- dplyr::left_join(CAR_time_point, RAT_report_time_point)
+
+        CAR_time_estimate <-  RAT_CAR_smooth |>
+            dplyr::mutate(test_prob_given_infection =
+                              zoo::na.locf(rat_reporting,
+                                           na.rm = FALSE,
+                                           fromLast = FALSE)) |>
+            dplyr::mutate(test_prob_given_infection = test_prob_given_symptom * proportion * 0.75)
+
+   } else { CAR_time_estimate <- CAR_time_point |>
+        dplyr::mutate(test_prob_given_infection = test_prob_given_symptom * 0.75)
+    }
 
     #join in the estimates
     CAR_smooth <- dplyr::left_join(tibble::tibble(date_state),
-                                   CAR_time_point)
+                                   CAR_time_estimate)
 
     #remove conditional on symptom column since it's linearly scaled from infection
     CAR_smooth <- CAR_smooth |>
-        dplyr::select(-test_prob_given_symptom)
+        dplyr::select(-test_prob_given_symptom, -fitted_point4)
 
-    last_perfection_ascertainment_date <- lubridate::as_date('2021_12_07')
+    #hack in 0.75 and 0.00 for Delta period for PCR and RAT
+   if(test_type == "PCR") {
+       pre_delta_ascertainment_date <- lubridate::as_date('2021_12_07')
+       pre_delta_ascertainment_rate <- 0.75
+   } else {
+        if (test_type == "RAT") {
+        pre_delta_ascertainment_date <- lubridate::as_date('2021_11_01')
+        pre_delta_ascertainment_rate <- 0.00
+            }}
 
-    #hack in 0.75 for Delta period
-    CAR_smooth <- CAR_smooth |>
+       CAR_smooth <- CAR_smooth |>
         dplyr::mutate(test_prob_given_infection =
                           dplyr::case_when(
-                              date <= last_perfection_ascertainment_date ~ 0.75,
+                              date <= pre_delta_ascertainment_date ~ pre_delta_ascertainment_rate,
                               TRUE ~ test_prob_given_infection))
 
-    #Add quick drop off in Dec 2021
+   #Add quick drop off in Dec 2021
     CAR_smooth <- CAR_smooth |>
         dplyr::mutate(test_prob_given_infection =
                           dplyr::case_when(
                               date == as.Date('2021-12-14') ~ 0.33,
                               TRUE ~ test_prob_given_infection))
+
+    if(test_type == "RAT") {
+        CAR_smooth <- CAR_smooth |>
+            dplyr::mutate(test_prob_given_infection =
+                              dplyr::case_when(
+                                  state == "VIC" & date > as.Date('2023-06-30') ~ 0.00,
+                                  state == "QLD" & date > as.Date('2023-08-31') ~ 0.00,
+                                  state == "NSW" & date > as.Date('2023-09-30') ~ 0.00,
+                                  TRUE ~ test_prob_given_infection))
+    }
 
     #interpolate NAs and repeat forward last survey result
     CAR_smooth <- CAR_smooth |>
