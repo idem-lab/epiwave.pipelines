cb48abbaa8858ad0b58abca572159ee9fb2dd6c4
AugustHao <tianxiao.hao@telethonkids.org.au>
2023-10-15 23:41:15 +0000

    polished up constant ascertainment input options

--- R/prepare_ascertainment_input.R
+++ R/prepare_ascertainment_input.R
@@ -2,35 +2,37 @@
 prepare_ascertainment_input <- function(
         dates, jurisdictions,
         ascertainment_estimate = NULL,
-        assume_constant_ascertainment = NULL, # should be value
+        constant_ascertainment = NULL, # should be value input if using
         test_type = c("PCR", "RAT")) {
 
     # add option here likelihood for seroprevalence data to help constrain prior
 
     #sanity check for ascertainment specification
     if (is.null(ascertainment_estimate) &
-        is.null(assume_constant_ascertainment)) {
+        is.null(constant_ascertainment)) {
 
         warning("Warning: not assuming constant ascertainment but no
                 ascertainment estimates provided, ascertainment rate with be
                 fitted with a naive prior")
 
     } else if (!is.null(ascertainment_estimate) &
-               !is.null(assume_constant_ascertainment)) {
+               !is.null(constant_ascertainment)) {
 
         warning("Warning: ascertainment estimates provided but overriden by
                 assumed constant ascertainment")
 
     } else if (is.null(ascertainment_estimate) &
-               !is.null(assume_constant_ascertainment)) {
-
-        ascertainment_input <- matrix(assume_constant_ascertainment,
+               !is.null(constant_ascertainment)) {
+        warning("using a constant assumed ascertainment value for every time
+                point and jurisdiction!")
+        ascertainment_input <- matrix(constant_ascertainment,
                                       nrow = length(dates),
                                       ncol = length(jurisdictions),
-                                      dimnames = list(dates, jurisdictions))
+                                      dimnames = list(as.character(dates),
+                                                      jurisdictions))
 
     } else if (!is.null(ascertainment_estimate) &
-               is.null(assume_constant_ascertainment)) {
+               is.null(constant_ascertainment)) {
 
         ascertainment_input <- get_CAR_from_surveys(
             dates, jurisdictions, ascertainment_estimate, test_type)
