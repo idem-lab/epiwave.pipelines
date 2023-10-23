7868b27880319441878564fec1f009e5fa783c03
AugustHao <tianxiao.hao@telethonkids.org.au>
2023-10-23 07:58:27 +0000

    implemented test type proportion correction and improved argument naming in notification data function

--- R/create_model_notification_data.R
+++ R/create_model_notification_data.R
@@ -1,31 +1,39 @@
 create_model_notification_data <- function(
         infections_timeseries,
         full_infection_dates,
-        observed_infection_dates,
+        observable_infection_dates,
         timevarying_delay_dist_ext,
         timevarying_proportion,
         observed_data,
         model_likelihood = 'negative_binomial',
         valid_mat = 1,
-        dataID) {
+        dataID,
+        case_type_proportion = 1) {
 
     n_days_infection <- nrow(infections_timeseries)
     n_jurisdictions <- ncol(infections_timeseries)
 
     # get indices for subset of infection days that end up in observed/forecast data
-    obs_infection_idx <- which(full_infection_dates %in% observed_infection_dates)
-
+    observable_infection_idx <- which(full_infection_dates %in% observable_infection_dates)
     # subset infection timeseries to these indices
-    infection_obs <- infections_timeseries[obs_infection_idx,]
+    infection_observable <- infections_timeseries[observable_infection_idx,]
     #note this is the number of infection days that get observed in the observed
-    #data, it is not the same length as the actual number of observed data,
-    #because we can observe (in the data) infections that had taken place before
-    #data collection
-    n_days_infection_obs <- length(obs_infection_idx)
+    #data, it is not the same length as the actual number of observed dates in
+    #notification series, which is below
+
+    #observed days in the obs data itself
+    #basically this backs out extra left and right days
+    obs_data_idx <- which(observable_infection_dates %in% as.Date(rownames(observed_data)))
+
+    #update proportion arg with type proportion
+    case_type_proportion <- pmax(case_type_proportion,1e-3)
+    timevarying_proportion[obs_data_idx,] <- timevarying_proportion[obs_data_idx,] * case_type_proportion
+
+    n_days_infection_observable <- length(observable_infection_idx)
 
     convolution_matrices <- lapply(1:n_jurisdictions, function(x)
         get_convolution_matrix(timevarying_delay_dist_ext[, x],
-                               n_days_infection_obs))
+                               n_days_infection_observable))
 
 
     # compute expected cases of the same length
@@ -33,7 +41,7 @@ create_model_notification_data <- function(
     expected_cases <- do.call(
         cbind,
         lapply(1:n_jurisdictions, function(x) {
-            convolution_matrices[[x]] %*% infection_obs[, x] *
+            convolution_matrices[[x]] %*% infection_observable[, x] *
                 timevarying_proportion[, x]
         }))
 
@@ -54,16 +62,12 @@ create_model_notification_data <- function(
             valid_idx <- which(valid_mat,arr.ind = FALSE)
         }
 
-        #observed days in the data itself
-        #basically this backs out extra left and right days
-        obs_data_idx <- which(observed_infection_dates %in% as.Date(rownames(observed_data)))
-
         # negative binomial parameters - need to change from mean and variance
         # specification to size and prob
         sqrt_inv_size <- normal(0, 0.5,
                                 truncation = c(0, Inf),
                                 dim = n_jurisdictions)
-        sqrt_inv_size <- sweep(greta::zeros(n_days_infection_obs,
+        sqrt_inv_size <- sweep(greta::zeros(n_days_infection_observable,
                                             n_jurisdictions),
                                2, sqrt_inv_size,
                                FUN = "+")
