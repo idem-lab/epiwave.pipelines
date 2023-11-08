b63fb7a0af1f1737b20dc73e3770850e63599ab1
Saras Windecker <saras.windecker@gmail.com>
2023-11-08 15:27:55 +1000

    notes on comple matrix problems

--- R/create_infection_compl_mat.R
+++ R/create_infection_compl_mat.R
@@ -1,13 +1,14 @@
 create_infection_compl_mat <- function (convolution_matrices,
                                         jurisdictions) {
 
-    # with_named_dates <- lapply(convolution_matrices, function(x) {
-    #     row.names(x) <- as.character(infection_days)
-    #     colnames(x) <- as.character(infection_days)
-    #     x
-    # })
     with_named_jurisdictions <- setNames(convolution_matrices, #with_named_dates,
                                          jurisdictions)
+
+    # we want proportion infections on that day that would be observed
+    # we don't have
+    # because this is centered on notification dates not infection dates
+    # it tells us amount of cases notified to that date that we have a chance of observing
+    # proportions of cases observable...
     inf_compl_prob_mat <- do.call(cbind, lapply(with_named_jurisdictions, colSums))
     inf_compl_prob_mat
 }
