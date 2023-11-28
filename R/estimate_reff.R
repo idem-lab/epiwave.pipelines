4a46d606b161c59abce4e6054dcd28b659e72678
Saras Windecker <saras.windecker@gmail.com>
2023-11-29 09:48:58 +1000

    modify timeseries object name

--- R/estimate_reff.R
+++ R/estimate_reff.R
@@ -1,11 +1,11 @@
-estimate_reff <- function (infections_timeseries,
+estimate_reff <- function (infection_timeseries,
                            generation_interval_mass_fxns) {
 
     convolution_matrix <- get_convolution_matrix(
-        generation_interval_mass_fxns, nrow(infections_timeseries))
-    infectiousness <- convolution_matrix %*% infections_timeseries
+        generation_interval_mass_fxns, nrow(infection_timeseries))
+    infectiousness <- convolution_matrix %*% infection_timeseries
 
-    reff <- infections_timeseries / (infectiousness + .Machine$double.eps)
+    reff <- infection_timeseries / (infectiousness + .Machine$double.eps)
 
     greta_arrays <- module(
         infectiousness,
